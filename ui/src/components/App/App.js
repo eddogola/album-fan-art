641fd8e67737bd832e603de9ea44c9788ee45bfa
Eddy Ogola Onyango <ogolaedd@fb.com>
2022-07-06 12:36:42 -0700

    Move react home page to Home component

--- ui/src/components/App/App.js
+++ ui/src/components/App/App.js
@@ -1,24 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from '../Home/Home';
+import SignIn from '../SignIn/SignIn';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={ <Home /> } />
+          <Route path='/auth' element={ <SignIn /> } />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
