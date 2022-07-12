4ef8adcab90baeada63aec1a2f0a41c295d318af
Eddy Ogola Onyango <ogolaedd@fb.com>
2022-07-12 11:30:00 -0700

    setAuth on App component load

--- ui/src/components/App/App.js
+++ ui/src/components/App/App.js
@@ -1,4 +1,5 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import Home from '../Home/Home';
 import Loading from '../Loading/Loading'
 import SignIn from '../SignIn/SignIn';
@@ -6,13 +7,20 @@ import './App.css';
 
 function App() {
   const [auth, setAuth] = useState(null);
+
+  useEffect(() => {
+    axios.get('/auth/current-session').then(({data}) => {
+      setAuth(data);
+    });
+  }, [])
+
   // if not logged in, ask to login
   if (auth === null) {
     return <Loading />
   }
   // if logged in, show canvas page
   if (auth) {
-    return <Home />
+    return <Home auth={ auth } />
   }
   // else ask user to sign in
   return <SignIn />
