e3496b445d07af054b82e71c41bd41c3507c5fac
Eddy Ogola Onyango <ogolaedd@fb.com>
2022-07-12 10:58:05 -0700

    Read secret values from .env

--- api/app.js
+++ api/app.js
@@ -6,10 +6,19 @@ const session = require('cookie-session');
 const helmet = require('helmet');
 const hpp = require('hpp');
 const csurf = require('csurf');
+const path = require('path');
+const dotenv = require('dotenv');
+
+// setup .env config
+dotenv.config({path: path.resolve(__dirname, '.env')});
+// import passport
+const passport = require('./passport');
 
 const app = express();
 
-app.use(cors());
+app.use(passport.initialize());
+
+// app.use(cors());
 app.use(morgan('tiny'));
 app.use(bodyParser.json());
 
@@ -21,7 +30,7 @@ app.use(hpp());
 app.use(
 	session({
 		name: 'session',
-		secret: 'gCoAMUj8FwPG9CY6',
+		secret: process.env.COOKIE_SECRET,
 		expires: new Date(Date.now() + 24 * 60 * 60 * 1000), // 24 hours
 	})
 );
