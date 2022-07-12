175a4265658871857c77f551b1da765ae52e9450
Eddy Ogola Onyango <ogolaedd@fb.com>
2022-07-12 10:58:20 -0700

    Create auth routes

--- api/routes/auth.js
+++ api/routes/auth.js
@@ -1,14 +1,39 @@
-const { PrismaClient, prisma } = require('@prisma/client');
-
 const express = require('express');
+const passport = require('passport');
+const jwt = require('jsonwebtoken');
+
+const jwtRequired = passport.authenticate('jwt', { session: false });
+
 const router = express.Router();
 
-require('dotenv').config();
+router.get('/login', passport.authenticate('auth0', {
+    scope: 'openid email profile',
+    }),
+    (req, res) => {
+        res.redirect('/');
+    }
+);
 
-router.post('/login', (req, res, next) => {
-    
-})
+router.get('/callback', (req, res, next) => {
+    passport.authenticate('auth0', (err, user) => {
+        if (err) {
+            return next(err);
+        }
+        if (!user) {
+            return res.redirect('/login');
+        }
+        console.log('CALLBACK SUCCESSFUL');
+        const userReturnObject = {
+            nickname: user.nickname,
+        };
+        req.session.jwt = jwt.sign(userReturnObject, process.env.JWT_SECRET_KEY);
+        return res.redirect('/');
+    })(req, res, next);
 
-const db = new PrismaClient();
+});
+
+router.get('/logout', (req, res) => {
+
+});
 
 module.exports = router;
