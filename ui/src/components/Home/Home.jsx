c85b2f42a028ba9a157fd3edc5534036b8d75c63
Eddy Ogola Onyango <ogolaedd@fb.com>
2022-07-12 11:28:58 -0700

    Show username on successful login

--- ui/src/components/Home/Home.jsx
+++ ui/src/components/Home/Home.jsx
@@ -1,12 +1,12 @@
 import logo from './logo.svg';
 import './Home.css';
 
-export default function Home(props) {
+export default function Home({ auth }) {
     return (
         <div className="Home">
             <header className="App-header">
                 <img src={logo} className="App-logo" alt="logo" />
-                <p>You are logged in :)</p>
+                <p>You are logged in as {auth && auth.nickname ? auth.nickname : null} :)</p>
                 <a className='App-header' href={ "/auth/logout" }>Logout</a>
             </header>
         </div>
