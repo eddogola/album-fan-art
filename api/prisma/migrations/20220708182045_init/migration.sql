f480fbf1f915926984ddc8111b47bb3da0e7ff5b
Eddy Ogola Onyango <ogolaedd@fb.com>
2022-07-12 10:57:10 -0700

    Remove prisma

--- api/prisma/migrations/20220708182045_init/migration.sql
+++
@@ -1,9 +0,0 @@
--- CreateTable
-CREATE TABLE "User" (
-    "id" SERIAL NOT NULL,
-    "name" VARCHAR(255) NOT NULL,
-    "email" VARCHAR(255) NOT NULL,
-    "password" VARCHAR(255) NOT NULL,
-
-    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
-);
