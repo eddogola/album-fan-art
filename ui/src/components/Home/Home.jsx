bb9f3274c003653844fc37e70572dd1360acd199
Eddy Ogola Onyango <ogolaedd@fb.com>
2022-07-15 14:19:15 -0700

    Add canvas and make sidebar scrollable

--- ui/src/components/Home/Home.jsx
+++ ui/src/components/Home/Home.jsx
@@ -1,5 +1,6 @@
 import './Home.css';
 import Sidebar from '../Sidebar/Sidebar';
+import Canvas from '../Canvas/Canvas';
 
 export default function Home({ auth }) {
     return (
@@ -8,6 +9,7 @@ export default function Home({ auth }) {
             <main className="canvas-container">
                 <h1>You are logged in as {auth && auth.nickname ? auth.nickname : null} :)</h1>
                 <h1><a className='App-header' href={ "/auth/logout" }>Logout</a></h1>
+                <Canvas />
             </main>
         </div>
     )
