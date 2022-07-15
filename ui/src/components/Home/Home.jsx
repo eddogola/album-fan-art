50ae020afc151ca430d8a7d213e71e1133dc9c2d
Eddy Ogola Onyango <ogolaedd@fb.com>
2022-07-15 14:02:25 -0700

    Create basic UI for canvas home page

--- ui/src/components/Home/Home.jsx
+++ ui/src/components/Home/Home.jsx
@@ -1,15 +1,13 @@
-import logo from './logo.svg';
 import './Home.css';
 import Sidebar from '../Sidebar/Sidebar';
 
 export default function Home({ auth }) {
     return (
-        <div className="Home">
+        <div className="home">
             <Sidebar />
-            <main className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-                <p>You are logged in as {auth && auth.nickname ? auth.nickname : null} :)</p>
-                <a className='App-header' href={ "/auth/logout" }>Logout</a>
+            <main className="canvas-container">
+                <h1>You are logged in as {auth && auth.nickname ? auth.nickname : null} :)</h1>
+                <h1><a className='App-header' href={ "/auth/logout" }>Logout</a></h1>
             </main>
         </div>
     )
--- ui/src/components/Sidebar/Sidebar.jsx
+++ ui/src/components/Sidebar/Sidebar.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import './Sidebar.css';
 import { createApi } from 'unsplash-js';
 import ImageGrid from '../ImageGrid/ImageGrid';
 
