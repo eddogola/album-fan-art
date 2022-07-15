8b5a7b216d083fa03538276e00185ceb3d07a246
Eddy Ogola Onyango <ogolaedd@fb.com>
2022-07-15 14:12:21 -0700

    Show images

--- ui/src/components/Image/Image.jsx
+++ ui/src/components/Image/Image.jsx
@@ -4,7 +4,7 @@ import './Image.css';
 export default function Image({ photo }) {
     return (
         <div className="image">
-            {/* <img className="img-thumbnail" src={photo.links.download + "?force=true"} alt={ photo.alt_description } /> */}
+            <img className="img-thumbnail" src={photo.links.download + "?force=true"} alt={ photo.alt_description } />
         </div>
     )
 }
