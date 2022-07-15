9ad165cfa6c7b25e59c1297b542addb1bd1fd750
Eddy Ogola Onyango <ogolaedd@fb.com>
2022-07-15 11:12:29 -0700

    Read unsplash api secret values from .env

--- ui/src/components/Sidebar/Sidebar.jsx
+++ ui/src/components/Sidebar/Sidebar.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
+import Unsplash, { toJSON } from 'unsplash-js';
 
-export default function Sidebar({}) {
+const unsplash = new Unsplash({
+    applicationId: process.env.REACT_APP_UNSPLASH_ACCESS_KEY,
+    secret: process.env.REACT_APP_UNSPLASH_SECRET_KEY,
+    }
+)
+
+export default function Sidebar(props) {
     return (
         <div className="sidebar">
-            
         </div>
     )
 }
