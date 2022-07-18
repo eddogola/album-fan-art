74c53cd96d944b7c710fa55b5399c8400cbbb505
Eddy Ogola Onyango <ogolaedd@fb.com>
2022-07-18 02:39:56 -0700

    Add delete object and add text buttons

--- ui/src/components/TextBar/TextBar.jsx
+++ ui/src/components/TextBar/TextBar.jsx
@@ -1,8 +1,11 @@
 import React from "react";
+import './TextBar.css';
 
-export default function TextBar({  }) {
+export default function TextBar({ onAddText, onDelete }) {
     return (
         <div className="text-bar">
+            <button onClick={ onAddText }>Add text</button>
+            <button onClick={ onDelete }>Delete</button>
         </div>
     )
 }
