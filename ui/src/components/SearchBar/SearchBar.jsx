7c86bc1f370c433a380754d763a2f8a7e75ee6d6
Eddy Ogola Onyango <ogolaedd@fb.com>
2022-07-18 12:39:11 -0700

    Set up infinite sidebar scroll

--- ui/src/components/SearchBar/SearchBar.jsx
+++ ui/src/components/SearchBar/SearchBar.jsx
@@ -1,11 +1,16 @@
 import React from "react";
 import './SearchBar.css';
 
-export default function SearchBar({  }) {
+export default function SearchBar({ setQuery, setPhotos, setIsLoading }) {
     return (
         <div className="search-bar">
             <form method="get">
-                <input type="search" name="search-bar" id="search-bar" placeholder="Search photo" />
+                <input type="search" name="search-bar" id="search-bar" placeholder="Search photo"
+                onChange={ (event) => {
+                    setQuery(event.target.value);
+                    setIsLoading(true);
+                    setPhotos([]);
+                } } />
             </form>
         </div>
     )
--- ui/src/components/Sidebar/Sidebar.jsx
+++ ui/src/components/Sidebar/Sidebar.jsx
@@ -1,8 +1,9 @@
 import React, { useState, useEffect } from 'react';
-import './Sidebar.css';
+import InfiniteScroll from "react-infinite-scroll-component";
 import { createApi } from 'unsplash-js';
 import SearchBar from '../SearchBar/SearchBar';
 import ImageGrid from '../ImageGrid/ImageGrid';
+import './Sidebar.css';
 
 const unsplash = createApi({
     accessKey: process.env.REACT_APP_UNSPLASH_ACCESS_KEY,
@@ -11,26 +12,52 @@ const unsplash = createApi({
 
 export default function Sidebar({ onClickImage }) {
     const [photos, setPhotos] = useState([]);
+    const [query, setQuery] = useState("new");
+    const [page, setPage] = useState(1);
+    const [isLoading, setIsLoading] = useState(true);
+    const [total, setTotal] = useState("");
+    const perPage = 40;
 
-    useEffect(() => {
+    const getMorePhotos = async (page) => {
         try {
-            unsplash.search
+            await unsplash.search
                 .getPhotos({
-                    query: "music",
+                    query: query,
+                    page: page,
+                    per_page: perPage,
                 })
                 .then(result => {
-                    photos.length ? 
-          setPhotos([...photos, ...result.response.results]) : setPhotos(result.response.results);
-                })
+                    setTotal(result.response.total);
+                    setPage(page);
+                    photos.length ? setPhotos([...photos, ...result.response.results]) : setPhotos(result.response.results)
+                });
         } catch(err) {
-            console.log("Unable to retrieve photos. Reason: ", err);
+            console.log("Unable to retrieve photos: ", err)
         }
-    }, [photos])
+    }
+
+    useEffect(() => {
+        setPage(1);
+        getMorePhotos(1);
+    }, [query])
 
     return (
         <div className="sidebar">
-            <SearchBar />
-            <ImageGrid photos={ photos } onClickImage={ onClickImage } />
+            <SearchBar setQuery={ setQuery } setPhotos={ setPhotos } setIsLoading={ setIsLoading } />
+            <InfiniteScroll
+                dataLength={ photos.length }
+                next={ () => getMorePhotos(page + 1) }
+                hasMore={ isLoading }
+            >
+                <ImageGrid photos={ photos } onClickImage={ onClickImage } />
+                { photos.length === total && total!== 0 ? 
+                (
+                    <p>You've seen it all</p>
+                ) : "" }
+            </InfiniteScroll>
+            <button className="topButton" onClick={() => {
+
+            }}>^</button>
         </div>
     )
 }
