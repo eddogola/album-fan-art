0c0721dc6ad904ee68b401ccae11254ef232e3d1
Eddy Ogola Onyango <ogolaedd@fb.com>
2022-07-18 02:39:28 -0700

    Remove text bar from main container

--- ui/src/components/MainContainer/MainContainer.jsx
+++ ui/src/components/MainContainer/MainContainer.jsx
@@ -1,12 +1,10 @@
 import React from "react";
-import TextBar from "../TextBar/TextBar";
 import WorkingArea from "../WorkingArea/WorkingArea";
 import './MainContainer.css';
 
 export default function MainContainer({ onReady, editor }) {
     return (
         <div className="main-container">
-            <TextBar />
             <WorkingArea onReady={ onReady } editor={ editor } />
         </div>
     )
