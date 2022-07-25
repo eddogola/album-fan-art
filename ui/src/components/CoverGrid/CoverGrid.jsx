b3f7c76b5f67614f7c0c0f529ba372f7eea0bb84
Eddy Ogola Onyango <ogolaedd@fb.com>
2022-07-24 22:07:22 -0700

    Create CoverGrid component

---
+++ ui/src/components/CoverGrid/CoverGrid.jsx
@@ -0,0 +1,33 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import Cover from '../Cover/Cover';
+
+export default function CoverGrid({ auth }) {
+    const [covers, setCovers] = useState([]);
+
+    const getCovers = (nickname) => {
+        axios.get('http://localhost:3001/covers', {
+            params: {
+                nickname: nickname,
+            },
+        })
+            .then((response) => {
+                setCovers(response.data);
+            }).catch(err => console.log("Error retrieving user covers:", err))
+    }
+
+    useEffect(
+        () => getCovers(auth.nickname),
+        [auth]
+    )
+
+    return (
+        <div className="cover-grid">
+            {/* return user covers */}
+            {/* hit `covers` endpoint */}
+            {
+                covers.map((cover, idx) => <Cover b64Img={ cover.body } key={'cover' + idx} />)
+            }
+        </div>
+    )
+}
