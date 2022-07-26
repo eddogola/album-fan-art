4ab0e75c849ce7530365231fb26518927988367d
Eddy Ogola Onyango <ogolaedd@fb.com>
2022-07-26 10:09:43 -0700

    Style image grid

--- ui/src/components/ImageGrid/ImageGrid.jsx
+++ ui/src/components/ImageGrid/ImageGrid.jsx
@@ -4,7 +4,7 @@ import Image from "../Image/Image";
 
 export default function ImageGrid({ photos, onClickImage }) {
     return (
-        <div className="image-grid">
+        <div className="image-grid row">
             { photos.map((photo, idx) => <Image onClick={ onClickImage } photo={ photo } id={idx} key={`image-${idx}`} />) }
         </div>
     )
