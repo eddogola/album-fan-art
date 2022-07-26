e1a234f73a8c7df1070c7dc9a8f174dfdd1ae04d
Eddy Ogola Onyango <ogolaedd@fb.com>
2022-07-26 09:10:50 -0700

    Style search bar

--- ui/src/components/SearchBar/SearchBar.jsx
+++ ui/src/components/SearchBar/SearchBar.jsx
@@ -6,17 +6,19 @@ export default function SearchBar({ setQuery, setPhotos, setIsLoading, formRef }
     return (
         <div className="search-bar">
             <form method="get" ref={ formRef }>
-                <input type="search" name="search-bar" id="search-bar" placeholder="Search photo"
-                onChange={ (event) => {
-                    if (event.target.value !== "") {
-                        setQuery(event.target.value);
-                        setIsLoading(true);
-                        setPhotos([]);
-                    } else {
-                        setQuery("album");
-                        setPhotos([]);
-                    }
-                } } />
+                <div className="input-group">
+                    <input type="search" name="search-bar" id="search-bar" placeholder="Search photo ..."
+                    onChange={ (event) => {
+                        if (event.target.value !== "") {
+                            setQuery(event.target.value);
+                            setIsLoading(true);
+                            setPhotos([]);
+                        } else {
+                            setQuery("album");
+                            setPhotos([]);
+                        }
+                    } } />
+                </div>
             </form>
         </div>
     )
