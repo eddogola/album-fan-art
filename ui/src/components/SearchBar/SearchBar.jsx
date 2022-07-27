cfc7a891bfe76abff4080aa2ec1179164ca319df
Eddy Ogola Onyango <ogolaedd@fb.com>
2022-07-26 21:18:49 -0700

    Add search icon to search bar and style

--- ui/src/components/SearchBar/SearchBar.jsx
+++ ui/src/components/SearchBar/SearchBar.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import 'bootstrap-icons/font/bootstrap-icons.css'
 import './SearchBar.css';
 
 export default function SearchBar({ setQuery, setPhotos, setIsLoading, formRef }) {
@@ -6,8 +7,8 @@ export default function SearchBar({ setQuery, setPhotos, setIsLoading, formRef }
     return (
         <div className="search-bar">
             <form method="get" ref={ formRef }>
-                <div className="input-group">
-                    <input type="search" name="search-bar" id="search-bar" placeholder="Search photo ..."
+                <div className="input-group ">
+                    <input type="search" className="form-control border-end-0 border" name="search-bar" id="search-bar" placeholder="Search photo ..."
                     onChange={ (event) => {
                         if (event.target.value !== "") {
                             setQuery(event.target.value);
@@ -18,6 +19,11 @@ export default function SearchBar({ setQuery, setPhotos, setIsLoading, formRef }
                             setPhotos([]);
                         }
                     } } />
+                    <span className="input-group-append">
+                    <button className="btn btn-outline-secondary bg-white border-start-0 border-bottom-0 border ms-n5" type="button">
+                        <i className="bi bi-search"></i>
+                    </button>
+                    </span>
                 </div>
             </form>
         </div>
