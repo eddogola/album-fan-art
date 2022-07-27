5c57a602d1eadaa2143a4fc58a275cd0b45c1100
Eddy Ogola Onyango <ogolaedd@fb.com>
2022-07-26 21:20:15 -0700

    Add signout button

--- ui/src/components/TextBar/TextBar.jsx
+++ ui/src/components/TextBar/TextBar.jsx
@@ -1,23 +1,31 @@
 import React from "react";
 import { Link } from 'react-router-dom';
+import 'bootstrap-icons/font/bootstrap-icons.css'
 import './TextBar.css';
 
-export default function TextBar({ onAddText, onDelete }) {
+export default function TextBar({ onAddText, onDelete, onLogout }) {
     return (
-        <div className="text-bar">
-            <button onClick={ onAddText }>Add text</button>
-            <Link to="covers">Your covers</Link>
-            {/* Load fonts */}
-            <p style={ {fontFamily: 'cursive'} }>cursive</p>
-            <select name="font-families" className="font-families" id="font-families">
-                <optgroup style={ {fontFamily: 'cursive'} }>
-                    <option value="cursive" >Cursive</option>
-                </optgroup>
-            </select>
-            {/* Load font sizes */}
-            {/* Load font weights */}
-            {/* Load colors */}
-            <button onClick={ onDelete }>Delete</button>
+        <div className="text-bar row">
+            <div className="col-md-4">
+                <Link className="btn btn-secondary" to="covers">Your covers</Link>
+            </div>
+            <div className="col-md-4 offset-md-4">
+                <button className="Btn btn btn-light" onClick={ onAddText }>Add text</button>
+                {/* Load fonts */}
+                {/* Load font sizes */}
+                {/* Load font weights */}
+                {/* Load colors */}
+                <button className="Btn btn btn-danger" onClick={ onDelete }>Delete</button>
+                <a className="btn btn-primary" 
+                    href="http://localhost:3001/auth/logout"
+                    data-bs-toggle="tooltip" 
+                    data-bs-placement="bottom"
+                    data-trigger="hover" 
+                    title="Sign Out">
+                        <i className="bi bi-box-arrow-right"></i>
+                </a>
+            </div>
+            
         </div>
     )
 }
