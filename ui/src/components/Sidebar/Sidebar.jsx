4a3d93dd58a0718c06e1fc3eb3db4da9babfb814
Eddy Ogola Onyango <ogolaedd@fb.com>
2022-07-26 21:19:06 -0700

    Accept only landscape pictures from unsplash

--- ui/src/components/Sidebar/Sidebar.jsx
+++ ui/src/components/Sidebar/Sidebar.jsx
@@ -25,6 +25,7 @@ export default function Sidebar({ onClickImage }) {
                     query: query,
                     page: page,
                     per_page: perPage,
+                    orientation: 'landscape',
                 })
                 .then(result => {
                     setTotal(result.response.total);
@@ -43,7 +44,7 @@ export default function Sidebar({ onClickImage }) {
 
     const gallery = <div className="gallery">
         <ImageGrid photos={ photos } onClickImage={ onClickImage } />
-        { ( photos.length === total && total!== 0 ? (<p>You've seen it all</p>) : "" ) }
+        { ( photos.length === total && total!== 0 ? (<b className='text-light text-center'>You've seen it all!</b>) : "" ) }
     </div>
 
     const formRef = useRef();
@@ -57,7 +58,7 @@ export default function Sidebar({ onClickImage }) {
                 hasMore={ isLoading }
                 scrollableTarget="sidebar"
             >
-             { photos.length > 0 ? gallery : <p>Sorry, there are no results for <strong>{ query }</strong>, try again!</p> }   
+             { photos.length > 0 ? gallery : <p className='text-light text-center'>Sorry, there are no results for <strong>{ query }</strong>, try again!</p> }   
             </InfiniteScroll>
             <button className="topButton" onClick={() => {
                 formRef.current.scrollIntoView({behaviour: 'smooth'});
