cf8bef261fa4f613e6b636a6deed528549ad2752
Eddy Ogola Onyango <ogolaedd@fb.com>
2022-07-26 21:13:19 -0700

    Add download icon to footer

--- ui/src/components/Footer/Footer.jsx
+++ ui/src/components/Footer/Footer.jsx
@@ -1,10 +1,15 @@
 import React from "react";
+import 'bootstrap-icons/font/bootstrap-icons.css';
 import './Footer.css'
 
 export default function Footer({ onClick }) {
     return (
         <div className="footer">
-            <span>Save image(download + cloud) <button onClick={() => onClick()}>save</button></span>
+            <span onClick={ onClick }>
+                <button className="download">
+                    Save album art <i className="bi bi-download"></i>
+                </button>
+            </span>
         </div>
     )
 }
