04d073bcb2489ff4a606701e0edcf0a2ee0ad1d9
Eddy Ogola Onyango <ogolaedd@fb.com>
2022-07-26 21:14:57 -0700

    Add TextBar within MainContainer

--- ui/src/components/MainContainer/MainContainer.jsx
+++ ui/src/components/MainContainer/MainContainer.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import WorkingArea from "../WorkingArea/WorkingArea";
+import TextBar from "../TextBar/TextBar";
 import './MainContainer.css';
 
-export default function MainContainer({ onReady, editor }) {
+export default function MainContainer({ onReady, editor, onAddText, onDelete, onLogout }) {
     return (
         <div className="main-container">
+            <TextBar onAddText={onAddText} onDelete={onDelete} onLogout={ onLogout } />
             <WorkingArea onReady={ onReady } editor={ editor } />
         </div>
     )
