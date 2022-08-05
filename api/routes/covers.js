13a91cad7e32fbc31b7662d9e13efa91c5e0a822
Eddy Ogola Onyango <ogolaedd@fb.com>
2022-08-05 12:50:21 -0700

    Add eslint

--- api/routes/covers.js
+++ api/routes/covers.js
@@ -1,39 +1,41 @@
-const { PrismaClient } = require('@prisma/client');
+const { PrismaClient } = require("@prisma/client");
 
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
-const prisma = new PrismaClient()
+const prisma = new PrismaClient();
 
-router.get('/', async (req, res) => {
-    const runQueries = async () => {
-        // get user nickname from request params
-        const nickname = req.query.nickname;
+router.get("/", async (req, res) => {
+	const runQueries = async () => {
+		// get user nickname from request params
+		const nickname = req.query.nickname;
 
-        // get UserArt(user) using nickname
-        const user = await prisma.userArt.findUnique({
-            where: {
-                userNickname: nickname,
-            },
-        })
-        if (!user) {
+		// get UserArt(user) using nickname
+		const user = await prisma.userArt.findUnique({
+			where: {
+				userNickname: nickname,
+			},
+		});
+
+		// if (!user) {
             
-        }
-        // then get UserArtId(user id) from user
-        // get ArtWork(covers) using UserArtId
-        const covers = await prisma.artWork.findMany({
-            where: {
-                userArtId: user.id,
-            },
-        })
-        
-        return covers;
-    }
+		// }
 
-    const covers = await runQueries();
+		// then get UserArtId(user id) from user
+		// get ArtWork(covers) using UserArtId
+		const covers = await prisma.artWork.findMany({
+			where: {
+				userArtId: user.id,
+			},
+		});
+        
+		return covers;
+	};
+
+	const covers = await runQueries();
     
-    // return covers
-    res.send(covers)
-})
+	// return covers
+	res.send(covers);
+});
 
 module.exports = router;
