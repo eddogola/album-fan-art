78f81491d351aa13cb37dc88fe7f2041940d5921
Eddy Ogola Onyango <ogolaedd@fb.com>
2022-08-10 08:53:39 -0700

    Add new ui build

---
+++ api/build/precache-manifest.8fbfaa92fa5fb28d080060c79a4028ef.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "0c8217c284372e5440f2",
+    "url": "/static/css/main.ab7ef845.chunk.css"
+  },
+  {
+    "revision": "0c8217c284372e5440f2",
+    "url": "/static/js/main.54ec5129.chunk.js"
+  },
+  {
+    "revision": "f7716cb1bb30fd23e8f9",
+    "url": "/static/js/runtime~main.677df6fa.js"
+  },
+  {
+    "revision": "0f009a46a3de3251fa1a",
+    "url": "/static/css/2.06d14385.chunk.css"
+  },
+  {
+    "revision": "0f009a46a3de3251fa1a",
+    "url": "/static/js/2.0d249243.chunk.js"
+  },
+  {
+    "revision": "478f2d29196ac3025490",
+    "url": "/static/js/3.36b5d764.chunk.js"
+  },
+  {
+    "revision": "be6df3f0c9855796ae0c099ee4ca43ce",
+    "url": "/static/media/bootstrap-icons.be6df3f0.woff"
+  },
+  {
+    "revision": "31e1300d419245fd27614630601dc74d",
+    "url": "/static/media/bootstrap-icons.31e1300d.woff2"
+  },
+  {
+    "revision": "d7bd3aac2ca9f7c561c531cdd7f343f0",
+    "url": "/static/media/parental-advisory.d7bd3aac.png"
+  },
+  {
+    "revision": "ba585bc6d7d3ab820c55bb3a9175db03",
+    "url": "/static/media/undraw_happy_music_g6wc.ba585bc6.svg"
+  },
+  {
+    "revision": "ee44ba9b2d835157eda9c1bf3b52e4e3",
+    "url": "/index.html"
+  }
+];
