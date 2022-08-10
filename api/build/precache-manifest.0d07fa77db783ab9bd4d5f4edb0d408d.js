57c60f9503c7c8aff6eb8688367e67c1d7e20f68
Eddy Ogola Onyango <ogolaedd@fb.com>
2022-08-10 09:11:32 -0700

    Add ui build

---
+++ api/build/precache-manifest.0d07fa77db783ab9bd4d5f4edb0d408d.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = [
+  {
+    "revision": "f0c297b9c1d3bfbb618f",
+    "url": "/static/css/main.f42c6225.chunk.css"
+  },
+  {
+    "revision": "f0c297b9c1d3bfbb618f",
+    "url": "/static/js/main.eb993ee8.chunk.js"
+  },
+  {
+    "revision": "27e52753c6def50b5a8e",
+    "url": "/static/js/runtime~main.a4611867.js"
+  },
+  {
+    "revision": "093938142f095a5f0ad1",
+    "url": "/static/css/2.06d14385.chunk.css"
+  },
+  {
+    "revision": "093938142f095a5f0ad1",
+    "url": "/static/js/2.4f0cf17e.chunk.js"
+  },
+  {
+    "revision": "8da34c9ba81a78f87de7",
+    "url": "/static/js/3.b486bc89.chunk.js"
+  },
+  {
+    "revision": "31e1300d419245fd27614630601dc74d",
+    "url": "/static/media/bootstrap-icons.31e1300d.woff2"
+  },
+  {
+    "revision": "be6df3f0c9855796ae0c099ee4ca43ce",
+    "url": "/static/media/bootstrap-icons.be6df3f0.woff"
+  },
+  {
+    "revision": "d7628da6d8e0e59a105679d62cf67f4b",
+    "url": "/static/media/acrylic-painting.d7628da6.jpg"
+  },
+  {
+    "revision": "d7bd3aac2ca9f7c561c531cdd7f343f0",
+    "url": "/static/media/parental-advisory.d7bd3aac.png"
+  },
+  {
+    "revision": "fbeff4a0b2a975f6a4b8d87a0baf9dfe",
+    "url": "/static/media/mona_lisa.fbeff4a0.jpg"
+  },
+  {
+    "revision": "11d8037a379ae2cb58ed57ee752fee96",
+    "url": "/static/media/starry_night.11d8037a.jpeg"
+  },
+  {
+    "revision": "561d57a524cabd04c8d3654b31d1622c",
+    "url": "/static/media/kanagawa.561d57a5.jpg"
+  },
+  {
+    "revision": "accc9a6921584f469ef9a41f2f705741",
+    "url": "/static/media/screaming-man.accc9a69.jpeg"
+  },
+  {
+    "revision": "270ce340f7a1a4e55c5c5678f5a16f77",
+    "url": "/static/media/fragonard.270ce340.jpg"
+  },
+  {
+    "revision": "ba585bc6d7d3ab820c55bb3a9175db03",
+    "url": "/static/media/undraw_happy_music_g6wc.ba585bc6.svg"
+  },
+  {
+    "revision": "33aa4c6a71fedf336b7d8fa57e857283",
+    "url": "/index.html"
+  }
+];
